ae9dadc56f5a115ff070c64ff06c62af0e2e7cbd
Curtis Preston <wizordgamming@gmail.com>
2022-05-13 20:09:12 +1200

    angular route test

--- buildAndMove.sh
+++ buildAndMove.sh
@@ -1,8 +1,13 @@
+# build angular project
 cd Crown5Frontend
 ng build --prod --build-optimizer --baseHref="/static/"
 cd ..
+# clean out old build
 rm templates/index.html
 rm ./static/*
+# copy angular build to served dir
 cp -a ./Crown5Frontend/dist/crown5-frontend/. ./static
+# copy index to to template folder
 cp ./static/index.html ./templates/index.html
+# remove index from static folders
 rm static/index.html
