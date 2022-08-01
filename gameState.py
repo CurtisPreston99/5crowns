5c715977dde85041aefd960405ed7803311e4d88
Curtis Preston <wizordgamming@gmail.com>
2022-08-01 20:52:24 +1200

    add deck generation

--- gameState.py
+++ gameState.py
@@ -1,22 +1,42 @@
 import json 
+import random
+
+class card:
+    def __init__(self,suit,value):
+        self.suit = suit
+        self.value = value
+
+class playerState:
+    def __init__(self):
+        self.score = 0
+        self.cards = []
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+class Encoder(json.JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+
+suits = [
+    "Hearts",
+    "Diamonds",
+    "Clovers",
+    "Spades",
+    "Stars"
+]
 
 cards = []
-for i in range(0,5):
-    type = []
-    for e in range(3,13):
-        type.append(e)
-    cards.append(type)
-    
-CLOVER = 0
-HEART = 1
-SPADES = 2
-STARS = 3
-DIAMONDS = 4
 
+for i in suits:
+    for e in range(3,13):
+        cards.append(card(i,e))
+    
 class crowns5GameState:
     def __init__(self):
         self.state = {
-            'boardState':{'messageC':0},
+            'boardState':{'deck':[]},
             'playerState':[]
         }
         self.clients = []
@@ -34,19 +54,26 @@ class crowns5GameState:
         return json_object
     
     def parseMessage(self,message):
-        self.state['boardState']['messageC'] += 1
-        return message;
+        messageDict = json.loads(message)
+        self.startRound()
+        # if messageDict['command']=="start":
+        return message
+        
+    def startRound(self,round = 0):
+        nCards = round + 3
+
+        deck = cards.copy()
+        deck = [deck,deck]
+
+        deck = [item for sublist in deck for item in sublist]
+
+        for i in range(6):
+            deck.append(card(None,50))
+
+        random.shuffle(deck)
+        
+        self.state['deck'] = deck
 
 
-class playerState:
-    def __init__(self):
-        self.score = 0
-        self.card = []
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
 
-class Encoder(json.JSONEncoder):
-        def default(self, o):
-            return o.__dict__
